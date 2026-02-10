df37468eaea96c80d6db510dd9bd367863445f41
Emily Unsworth <unsworthemily@gmail.com>
2026-02-10 15:55:59 +0100

    Fix navbar layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="footer" id="contact">
       <div className="container footer__inner">
-        <p className="footermuted">R3: OnChange, Objects, and Arrays.</p>
+        <p className="footermuted">R3: OnChange, Objects, and Arrays & R4: Map, Some, and Filter</p>
         <div className="footer__links">
           <a href="https://github.com/unsworthemily" target="_blank" rel="noreferrer">GitHub</a>
           <a href="mailto:unsworth_emily@hotmail.com">Email</a>
