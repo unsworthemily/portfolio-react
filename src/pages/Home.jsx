b9671bb6fd223ef0780a9d8e517f1e10d11d438d
Emily Unsworth <unsworthemily@gmail.com>
2026-02-11 14:55:48 +0100

    Improve project layout width

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Skills />
     </>
   );
 }
