ec451a1d705cfdd7e2e87cc5c134b8e37195661a
Emily Unsworth <unsworthemily@gmail.com>
2026-02-16 15:03:33 +0100

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-react/", // <-- matcha namnet på repot
+  base: "/portfolio/",
 });
